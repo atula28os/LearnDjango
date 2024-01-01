20901f2bd624cd4acdd4668dcf1f9d1ce84e08f5
atula28os <atula28os@gmail.com>
2024-01-01 22:02:58 +0530

    Class Based Views and Serializers

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,11 +1,58 @@
-from rest_framework.response import Response
+
 from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 
 from rest_framework.decorators import api_view
 from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class MovieListAV(APIView):
+
+    def get(self, request):
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+class MovieDetailAV(APIView):
+
+    def get(self, request, pk):
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'message':f"Movie with ID {pk} doesn't exist"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        movie = Movie.objects.get(pk=pk) 
+        movie.delete()
+        content = {'message': 'movie deleted'}
+        return Response(content, status=status.HTTP_204_NO_CONTENT)
+
+
+"""
+
+#### FUNCTION BASED VIEWS #####
+
 @api_view(['GET', 'POST'])
 def movie_list(request):
     if request.method == 'GET':
@@ -43,3 +90,5 @@ def movie_details(request, pk):
         movie.delete()
         content = {'message': 'movie deleted'}
         return Response(content, status=status.HTTP_204_NO_CONTENT)
+"""
+
